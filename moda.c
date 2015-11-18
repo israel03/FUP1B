fc1d1852ec0b1a195a74b9a4e9e1787bdf553fa9
Arnulfo Diaz <ufoloky@gmail.com>
2015-11-18 01:06:16 -0600

    moda

---
+++ moda.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int calif;
+int moda[10];
+int i;
+
+int main()
+{
+	printf("Programa que calcula la moda de 10 calificaciones:\n");
+	
+	for(i=0;i<10;i++){
+		
+		
+		printf("Escriba la calificacion:\n");
+		scanf("%d", &calif);
+		moda[i]=0;
+		moda[calif["i"]-1]++;
+	
+	}
+	
+	printf("Moda: %d\n",calif);
+	
+	
+	
+	
+	
+}
